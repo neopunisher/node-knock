47c58249ac2ccbb8f202e2fbdf51e3c6a23cd00c
Carter Cole <carter@contactology.com>
2014-02-08 23:33:00 -0500

    newer version

--- knock.js
+++ knock.js
@@ -1,8 +1,10 @@
 #!/usr/bin/env node
 var argv = require('optimist').argv,
-knock = require('lib.js'),
+knock = require('./lib.js'),
 _ = require('underscore');
 
+_.defer(function(){
 _.each(argv._,function doKnock(domain){
 knock(domain);
+});
 });
--- lib.js
+++ lib.js
@@ -1,30 +1,49 @@
-var _ = require('underscore')._, glob = require('glob'),
-tldtools = require('tldtools').init(),
-dns = require('dns'), request = require('request'),
-Step = require('step'), fs = require('fs'),
-dnstest = function(){  },
-webtest = function(){  },
-basedomain = function(domain){ var res = tldtools.extract(domain).inspect; return res.useful()?res.getDomain():false; }
-module.exports.opts = {type:dnstest,types:[dnstest,webtest],verify:true,wildcard:{tests:1,neverthere:'knocknpmknocknpm'}};
+var _ = require('underscore')._,
+    glob = require('glob'),
+    tldtools = require('tldtools').init(function () {
+        //console.log('tldsready')
+    }),
+    dns = require('dns'),
+    request = require('request'),
+    Step = require('step'),
+    fs = require('fs'),
+    dnstest = function () {},
+    webtest = function () {},
+    basedomain = function (domain) {
+        if (domain.indexOf('http') != 0) {
+            domain = 'http://' + domain + '/'
+        }
+        var res = tldtools.extract(domain);
+        return res.inspect.useful() ? res.domain + '.' + res.tld : false;
+    }
+module.exports.opts = {
+    type: dnstest,
+    types: [dnstest, webtest],
+    verify: true,
+    wildcard: {
+        tests: 1,
+        neverthere: 'knocknpmknocknpm'
+    }
+};
 
 //dns.resolve4(domain, cb);
 
-module.exports = function knock(domain,opts,cb){
-if (_.isFunction(opts)){
-console.log('swaped for cb',module.exports.opts);
-cb = opts;
-opts = module.export.opts
-}else if(_.isOnject(opts)){
-opts = _.extend(module.exports.opts,opts)
-console.log('extended',opts)
-}
+module.exports = function knock(domain, opts, cb) {
+    if (_.isFunction(opts)) {
+        console.log('swaped for cb', module.exports.opts);
+        cb = opts;
+        opts = module.export.opts
+    } else if (_.isObject(opts)) {
+        opts = _.extend(module.exports.opts, opts)
+        console.log('extended', opts)
+    }
 
-var domain = basedomain(domain);
-if ( domain === false ) {
-cb('invalid domain');
-}else{
-console.log('basedomain',domain);
+    var domain = basedomain(domain);
+    if (domain === false) {
+        cb('invalid domain');
+    } else {
+        console.log('basedomain', domain);
 
 
-}
+    }
 }
